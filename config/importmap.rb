a8aa5e3a2db3275f640bd1809820f2bef1650fa1
Matt Menefee <matt.menefee@extractablemedia.com>
2024-01-31 12:08:10 -0700

    Add Cocooned v2.1.0

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,5 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin '@notus.sh/cocooned', to: 'https://cdn.jsdelivr.net/npm/@notus.sh/cocooned@2.1.0/index.js'
 pin_all_from "app/javascript/controllers", under: "controllers"
