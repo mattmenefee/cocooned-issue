b9c16f6c43faa3d86dc42929a45d02bd8b22377b
mattmenefee <mattmenefee@users.noreply.github.com>
2024-10-16 15:35:42 +0200

    Update the app's dependencies - October 2024
    * cocooned 2.2.1 → 2.4.0
    [changelog](https://github.com/notus-sh/cocooned/blob/main/CHANGELOG.md#version-240-2024-10-01)
    
    * i18n 1.14.5 → 1.14.6
    [changelog](https://github.com/ruby-i18n/i18n/releases)
    
    * importmap-rails 2.0.1 → 2.0.3
    
    * irb 1.14.0 → 1.14.1
    [changelog](https://github.com/ruby/irb/releases)
    
    * logger 1.6.0 → 1.6.1
    
    * msgpack 1.7.2 → 1.7.3
    
    * net-imap 0.4.14 → 0.4.17
    [changelog](https://github.com/ruby/net-imap/releases)
    
    * pg 1.5.7 → 1.5.8
    [changelog](https://github.com/ged/ruby-pg/blob/master/History.md#v158-2024-09-06-lars-kanis-larsgreiz-reinsdorfde)
    
    * rack 3.1.7 → 3.1.8
    [changelog](https://github.com/rack/rack/blob/main/CHANGELOG.md#318---2024-10-14)
    
    * redis 5.2.0 → 5.3.0
    [changelog](https://github.com/redis/redis-rb/blob/master/CHANGELOG.md#530)
    
    * reline 0.5.9 → 0.5.10
    [changelog](https://github.com/ruby/reline/releases)
    
    * rexml 3.3.6 → 3.3.8
    [changelog](https://github.com/ruby/rexml/releases/tag/v3.3.8)
    
    * rspec-core 3.13.0 → 3.13.1
    [changelog](https://github.com/rspec/rspec-core/blob/v3.13.1/Changelog.md#3131--2024-09-02)
    
    * rspec-expectations 3.13.2 → 3.13.3
    [changelog](https://github.com/rspec/rspec-expectations/blob/v3.13.3/Changelog.md#3133--2024-09-07)
    
    * rspec-mocks 3.13.1 → 3.13.2
    [changelog](https://github.com/rspec/rspec-mocks/blob/v3.13.2/Changelog.md#3132--2024-10-02)
    
    * rspec-rails 6.1.4 → 7.0.1
    [changelog](https://github.com/rspec/rspec-rails/blob/v7.0.1/Changelog.md#701--2024-09-03)
    
    * selenium-webdriver 4.23.0 → 4.25.0
    [changelog](https://github.com/SeleniumHQ/selenium/blob/trunk/rb/CHANGES)
    
    * thor 1.3.1 → 1.3.2
    [changelog](https://github.com/rails/thor/releases/tag/v1.3.2)
    
    * turbo-rails 2.0.6 → 2.0.11
    [changelog](https://github.com/hotwired/turbo-rails/releases)
    
    * zeitwerk 2.6.17 → 2.7.0
    [changelog](https://github.com/fxn/zeitwerk/blob/master/CHANGELOG.md#270-11-october-2024)

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
-pin "@notus.sh/cocooned", to: "@notus.sh--cocooned.js" # @2.2.1
+pin "@notus.sh/cocooned", to: "@notus.sh--cocooned.js" # @2.4.0
 pin_all_from "app/javascript/controllers", under: "controllers"
